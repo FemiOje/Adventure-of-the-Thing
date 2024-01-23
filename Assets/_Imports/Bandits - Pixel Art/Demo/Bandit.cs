5ce477e648afc366ab405f23faa75cbafe449767
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-01-23 21:01:58 +0100

    add: attack system

--- Assets/_Imports/Bandits - Pixel Art/Demo/Bandit.cs
+++ Assets/_Imports/Bandits - Pixel Art/Demo/Bandit.cs
@@ -1,72 +1,109 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bandit : MonoBehaviour {
+public class Bandit : MonoBehaviour
+{
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_Bandit       m_groundSensor;
-    private bool                m_grounded = false;
-    private bool                m_combatIdle = false;
-    private bool                m_isDead = false;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_Bandit m_groundSensor;
+    private bool m_grounded = false;
+    private bool m_combatIdle = false;
+    private bool m_isDead = false;
 
-    // Use this for initialization
-    void Start () {
+    //femi
+    private HeroKnight player;
+    private int health = 100;
+    private int damagePoints = 10;
+    public bool isAttacking;
+    private bool hasTakenDamageThisAttack;
+
+    //femi
+
+    private void Awake()
+    {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
-        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
+
+        player = GameObject.FindWithTag("Player").GetComponent<HeroKnight>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //Check if character just landed on the ground
-        if (!m_grounded && m_groundSensor.State()) {
+        if (!m_grounded && m_groundSensor.State())
+        {
             m_grounded = true;
             m_animator.SetBool("Grounded", m_grounded);
         }
 
         //Check if character just started falling
-        if(m_grounded && !m_groundSensor.State()) {
+        if (m_grounded && !m_groundSensor.State())
+        {
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
         }
 
-        // -- Handle input and movement --
+        // // -- Handle input and movement --
         float inputX = Input.GetAxis("Horizontal");
 
-        // Swap direction of sprite depending on walk direction
-        if (inputX > 0)
-            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        else if (inputX < 0)
-            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        // // Swap direction of sprite depending on walk direction
+        // if (inputX > 0)
+        //     transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        // else if (inputX < 0)
+        //     transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        // Calculate direction to move towards the player
+        if (player != null && player.health >= 0)
+        {
+            float direction = Mathf.Sign(player.transform.position.x - transform.position.x);
+
+            // Swap direction of sprite depending on walk direction
+            if (direction > 0)
+                transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            else if (direction < 0)
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+            // Move towards the player
+            m_body2d.velocity = new Vector2(direction * m_speed, 0f);
+        }
 
         // Move
-        m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
+        // m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
 
         //Set AirSpeed in animator
         m_animator.SetFloat("AirSpeed", m_body2d.velocity.y);
 
         // -- Handle Animations --
         //Death
-        if (Input.GetKeyDown("e")) {
-            if(!m_isDead)
+        if (Input.GetKeyDown("e"))
+        {
+            if (!m_isDead)
                 m_animator.SetTrigger("Death");
             else
                 m_animator.SetTrigger("Recover");
 
             m_isDead = !m_isDead;
         }
-            
+
         //Hurt
         else if (Input.GetKeyDown("q"))
             m_animator.SetTrigger("Hurt");
 
         //Attack
-        else if(Input.GetMouseButtonDown(0)) {
+        else if (Input.GetMouseButtonDown(0))
+        {
+            isAttacking = true;
             m_animator.SetTrigger("Attack");
+            hasTakenDamageThisAttack = false;
         }
 
         //Change between idle and combat idle
@@ -74,7 +111,8 @@ public class Bandit : MonoBehaviour {
             m_combatIdle = !m_combatIdle;
 
         //Jump
-        else if (Input.GetKeyDown("space") && m_grounded) {
+        else if (Input.GetKeyDown("space") && m_grounded)
+        {
             m_animator.SetTrigger("Jump");
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
@@ -93,5 +131,30 @@ public class Bandit : MonoBehaviour {
         //Idle
         else
             m_animator.SetInteger("AnimState", 0);
+
+         if (Vector2.Distance(player.transform.position, transform.position) <= 1.0f)
+        {
+            m_body2d.velocity = Vector3.zero;
+
+            if (player.isAttacking && !hasTakenDamageThisAttack)
+            {
+                m_animator.SetTrigger("Hurt");
+                health -= damagePoints;
+                hasTakenDamageThisAttack = true;
+            }
+        }
+
+        //destroy bandit when health is zero
+        if (health <= 0)
+        {
+            Debug.Log("Bandit destroyed");
+            m_animator.SetTrigger("Death");
+            m_body2d.velocity = Vector3.zero;
+            StartCoroutine(RemoveEnemy());
+        }
+    }
+    IEnumerator RemoveEnemy(){
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
     }
 }
--- Assets/_Imports/Hero Knight - Pixel Art/Demo/HeroKnight.cs
+++ Assets/_Imports/Hero Knight - Pixel Art/Demo/HeroKnight.cs
@@ -1,37 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeroKnight : MonoBehaviour {
+public class HeroKnight : MonoBehaviour
+{
 
-    [SerializeField] float      m_speed = 4.0f;
-    [SerializeField] float      m_jumpForce = 7.5f;
-    [SerializeField] float      m_rollForce = 6.0f;
-    [SerializeField] bool       m_noBlood = false;
+    [SerializeField] float m_speed = 4.0f;
+    [SerializeField] float m_jumpForce = 7.5f;
+    [SerializeField] float m_rollForce = 6.0f;
+    [SerializeField] bool m_noBlood = false;
     [SerializeField] GameObject m_slideDust;
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_HeroKnight   m_groundSensor;
-    private Sensor_HeroKnight   m_wallSensorR1;
-    private Sensor_HeroKnight   m_wallSensorR2;
-    private Sensor_HeroKnight   m_wallSensorL1;
-    private Sensor_HeroKnight   m_wallSensorL2;
-    private bool                m_isWallSliding = false;
-    private bool                m_grounded = false;
-    private bool                m_rolling = false;
-    private int                 m_facingDirection = 1;
-    private int                 m_currentAttack = 0;
-    private float               m_timeSinceAttack = 0.0f;
-    private float               m_delayToIdle = 0.0f;
-    private float               m_rollDuration = 8.0f / 14.0f;
-    private float               m_rollCurrentTime;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_HeroKnight m_groundSensor;
+    private Sensor_HeroKnight m_wallSensorR1;
+    private Sensor_HeroKnight m_wallSensorR2;
+    private Sensor_HeroKnight m_wallSensorL1;
+    private Sensor_HeroKnight m_wallSensorL2;
+    private bool m_isWallSliding = false;
+    private bool m_grounded = false;
+    private bool m_rolling = false;
+    private int m_facingDirection = 1;
+    private int m_currentAttack = 0;
+    private float m_timeSinceAttack = 0.0f;
+    private float m_delayToIdle = 0.0f;
+    private float m_rollDuration = 8.0f / 14.0f;
+    private float m_rollCurrentTime;
+
+    //<femi>
+    public int health;
+    private int damagePoints;
+    public bool isAttacking;
+    //</femi>
 
 
-    // Use this for initialization
-    void Start ()
+    private void Awake()
     {
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        health = 100;
+        damagePoints = 10;
+        isAttacking = false;
         m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_HeroKnight>();
         m_wallSensorR1 = transform.Find("WallSensor_R1").GetComponent<Sensor_HeroKnight>();
         m_wallSensorR2 = transform.Find("WallSensor_R2").GetComponent<Sensor_HeroKnight>();
@@ -40,17 +52,17 @@ public class HeroKnight : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         // Increase timer that controls attack combo
         m_timeSinceAttack += Time.deltaTime;
 
         // Increase timer that checks roll duration
-        if(m_rolling)
+        if (m_rolling)
             m_rollCurrentTime += Time.deltaTime;
 
         // Disable rolling if timer extends duration
-        if(m_rollCurrentTime > m_rollDuration)
+        if (m_rollCurrentTime > m_rollDuration)
             m_rolling = false;
 
         //Check if character just landed on the ground
@@ -76,7 +88,7 @@ public class HeroKnight : MonoBehaviour {
             GetComponent<SpriteRenderer>().flipX = false;
             m_facingDirection = 1;
         }
-            
+
         else if (inputX < 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
@@ -84,7 +96,7 @@ public class HeroKnight : MonoBehaviour {
         }
 
         // Move
-        if (!m_rolling )
+        if (!m_rolling)
             m_body2d.velocity = new Vector2(inputX * m_speed, m_body2d.velocity.y);
 
         //Set AirSpeed in animator
@@ -101,22 +113,19 @@ public class HeroKnight : MonoBehaviour {
             m_animator.SetBool("noBlood", m_noBlood);
             m_animator.SetTrigger("Death");
         }
-            
+
         //Hurt
         else if (Input.GetKeyDown("q") && !m_rolling)
             m_animator.SetTrigger("Hurt");
 
-        //Attack
-        else if(Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.25f && !m_rolling)
+        // Attack
+        else if (Input.GetMouseButtonDown(0) && m_timeSinceAttack > 0.5f && !m_rolling)
         {
+            isAttacking = true;
             m_currentAttack++;
 
-            // Loop back to one after third attack
+            // Loop back to one after the third attack
             if (m_currentAttack > 3)
-                m_currentAttack = 1;
-
-            // Reset Attack combo if time since last attack is too large
-            if (m_timeSinceAttack > 1.0f)
                 m_currentAttack = 1;
 
             // Call one of three attack animations "Attack1", "Attack2", "Attack3"
@@ -143,7 +152,7 @@ public class HeroKnight : MonoBehaviour {
             m_animator.SetTrigger("Roll");
             m_body2d.velocity = new Vector2(m_facingDirection * m_rollForce, m_body2d.velocity.y);
         }
-            
+
 
         //Jump
         else if (Input.GetKeyDown("space") && m_grounded && !m_rolling)
@@ -168,8 +177,20 @@ public class HeroKnight : MonoBehaviour {
         {
             // Prevents flickering transitions to idle
             m_delayToIdle -= Time.deltaTime;
-                if(m_delayToIdle < 0)
-                    m_animator.SetInteger("AnimState", 0);
+            if (m_delayToIdle < 0)
+                m_animator.SetInteger("AnimState", 0);
+        }
+
+        //make player immobile while attacking
+        if (isAttacking)
+        {
+            inputX = 0.0f;
+            m_body2d.velocity = Vector3.zero;
+        }
+
+        if (health <= 0)
+        {
+            m_animator.SetTrigger("Death");
         }
     }
 
@@ -191,5 +212,10 @@ public class HeroKnight : MonoBehaviour {
             // Turn arrow in correct direction
             dust.transform.localScale = new Vector3(m_facingDirection, 1, 1);
         }
+    }
+
+    void AE_EndAttack()
+    {
+        isAttacking = false;
     }
 }
