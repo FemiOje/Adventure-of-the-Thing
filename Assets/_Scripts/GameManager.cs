e57149e8e20002149efd18cb2bc4193b545819b1
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-01-24 19:00:52 +0100

    import tile asset for environment

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,15 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void Play(){
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PauseGame()
+    {
+        Debug.Log("Pause game");
+        Time.timeScale = 0.0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void LoadNextScene()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void Quit(){
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
